7535bac90ff14e733fbbf6f7ee24105f95f2110b
Carlos Jara <cjjara002@gmail.com>
2024-06-30 21:03:56 -0700

    working on the home component

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,31 +1,106 @@
 import { useTheme } from "@react-navigation/native";
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, View, Text, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { IMAGES } from "../constants/Theme";
+import { COLORS, FONTS, IMAGES } from "../constants/Theme";
 import { GlobalStyleSheet } from "../constants/StyleSheet";
+import React from "react";
+import Feather from "react-native-vector-icons/Feather";
+import {
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
-const Home = () => {
+const Home = (props: any) => {
+  // a powerful tool for applying consistent theming across your React Native app
+  const theme = useTheme();
+  const { colors } = theme;
 
-    // a powerful tool for applying consistent theming across your React Native app
-    const theme = useTheme();
-    const {colors} = theme;
-
-
-    return(
-        <>
+  return (
+    <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         {/* Avoids Obstructions and  Provides Consistent Layout*/}
-        <SafeAreaView style={{flex:1}}>
-          <View style={{paddingBottom: 10}}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={{ paddingBottom: 10 }}>
             <ImageBackground
-            source={theme.dark ? IMAGES.foodPattern2 : IMAGES.foodPattern1}
-            style={GlobalStyleSheet.headerArea}
+              source={theme.dark ? IMAGES.foodPattern2 : IMAGES.foodPattern1}
+              style={GlobalStyleSheet.headerArea}
             >
-
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Feather
+                  style={{ marginRight: 12 }}
+                  size={22}
+                  color={COLORS.white}
+                  name={"map-pin"}
+                />
+                <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text
+                      // The spread operator (...) is used to combine the properties of one object with another object.
+                      style={{
+                        ...FONTS.h6,
+                        lineHeight: 18,
+                        color: COLORS.white,
+                      }}
+                    >
+                      Work
+                    </Text>
+                    <Feather
+                      style={{ marginLeft: 2 }}
+                      size={20}
+                      color={COLORS.white}
+                      name={"chevron-down"}
+                    />
+                  </View>
+                  <Text
+                    numberOfLines={1}
+                    style={{
+                      ...FONTS.font,
+                      color: COLORS.white,
+                      opacity: 0.75,
+                    }}
+                  >
+                    Rkpuram sector-b near chawla circle...
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => props.navigation.openDrawer()}
+                  style={GlobalStyleSheet.NotificationBtn}
+                >
+                  <Feather
+                    style={{ marginLeft: -1 }}
+                    size={22}
+                    color={COLORS.white}
+                    name="grid"
+                  />
+                </TouchableOpacity>
+              </View>
             </ImageBackground>
+            <View style={{ marginHorizontal: 15, marginTop: -26 }}>
+              <View
+                style={[
+                  {
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: 4 },
+                    shadowOpacity: 0.15,
+                    shadowRadius: 5,
+                  },
+                  Platform.OS === "ios" && {
+                    backgroundColor: colors.card,
+                    borderRadius: 10,
+                  },
+                ]}
+              ></View>
+            </View>
           </View>
         </SafeAreaView>
-        </>
-    )
-}
+      </GestureHandlerRootView>
+    </>
+  );
+};
 
 export default Home;
--- constants/Theme.js
+++ constants/Theme.js
@@ -5,3 +5,21 @@
 	foodPattern2 : require('../assets/images/patterns/patterndark1.png'),
 	foodPattern3 : require('../assets/images/patterns//pattern3.png'),
     }
+
+    export const COLORS = {
+        white   : "#fff",
+        title : "#2F2F2F",
+        text : "rgba(0,0,0,.7)",
+    }
+
+    export const SIZES = {
+        font:14,
+        //Font Sizes
+        h6:16,
+    };
+
+    export const FONTS = {
+        font   : {fontSize:SIZES.font, color: COLORS.text, lineHeight:20, fontFamily:'NunitoSans-Regular'},
+
+        h6     : {fontSize:SIZES.h6, color:COLORS.title, fontFamily:'Poppins-SemiBold'},  
+    }
